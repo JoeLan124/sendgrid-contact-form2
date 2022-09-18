b138e8d8dc734ddf90300cc4e2bf396c79e6d12f
Johannes Langosch <johannes.langosch@googlemail.com>
2022-09-18 19:18:43 +0200

    10 next email syntax test

--- pages/api/sendemail.js
+++ pages/api/sendemail.js
@@ -2,11 +2,14 @@ import sendgrid from "@sendgrid/mail";
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
+const email_from1 = process.env.EMAIL_FROM;
+const email_to1 = process.env.EMAIL_TO;
+
 async function sendEmail(req, res) {
   try {
     await sendgrid.send({
-      to: `${process.env.EMAIL_TO}`, // Your email where you'll receive emails
-      from: `${process.env.EMAIL_FROM}`, // your website email address here
+      to: email_from1, // Your email where you'll receive emails
+      from: email_to1, // your website email address here
       subject: `Beantragung Berechtigung für ${req.body.surname} ${req.body.pnr}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="de">
